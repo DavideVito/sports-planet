629e5cb7d709c1003cf9056c0baca94cd62c9ebb
davideVitiello <lgvitiello9@gmail.com>
2021-05-01 12:58:48 +0200

    Middleware per i form che fa i controlli e tutto

--- src/components/Login/AggiungiInfo/OpzioneForm/index.jsx
+++ src/components/Login/AggiungiInfo/OpzioneForm/index.jsx
@@ -1,9 +1,67 @@
 import Errore from "../../../Errore";
 
-import Giocatore from "./Giocatore";
+import GiocatoreForm from "./Forms/GiocatoreForm";
+import AllenatoreForm from "./Forms/AllenatoreForm";
+import ScoutForm from "./Forms/ScoutForm";
+import SocietaForm from "./Forms/SocietaForm";
+import Opzione from "./Opzione";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+const sports = [
+  {
+    nome: "Pallavolo",
+  },
+  { nome: "Calcio" },
+  { nome: "Basket" },
+];
+
+const Wrapper = ({ sportSelezionato, setSportSelezionato }) => {
+  return (
+    <div>
+      Cosa ti interessa?
+      {sports.map((sport) => {
+        return (
+          <Opzione
+            nome={sport.nome}
+            setSelezionato={setSportSelezionato}
+            otherStyles={
+              sportSelezionato === sport.nome
+                ? { backgroundColor: "blue" }
+                : { backgroundColor: "lightgreen", marginLeft: "10px" }
+            }
+          />
+        );
+      })}
+    </div>
+  );
+};
+
 const OpzioneForm = ({ opzione }) => {
+  const [sportSelezionato, setSportSelezionato] = useState("");
+
   if (opzione === "Giocatore") {
-    return <Giocatore />;
+    return (
+      <div>
+        <Wrapper
+          sportSelezionato={sportSelezionato}
+          setSportSelezionato={setSportSelezionato}
+        />
+        <GiocatoreForm sportSelezionato={sportSelezionato} />
+      </div>
+    );
+  }
+
+  if (opzione === "Allenatore") {
+    return <AllenatoreForm />;
+  }
+  if (opzione === "Società") {
+    return <SocietaForm />;
+  }
+  if (opzione === "Osservatore") {
+    return <ScoutForm />;
+  }
+  if (opzione === "Tifoso") {
+    return <Link to="/home"> Vai alla home </Link>;
   }
   return <Errore titolo="Non esiste" messaggio="cazzo fai idota" />;
 };
