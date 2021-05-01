ad031906b715b36f71ddf26155e2fa6538d93ebd
davideVitiello <lgvitiello9@gmail.com>
2021-05-01 13:00:03 +0200

    From per la società

---
+++ src/components/Login/AggiungiInfo/OpzioneForm/Forms/SocietaForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import GenericInfoForm from "./GenericInfoForm";
+import MultiSelect from "react-multi-select-component";
+
+const stabilimenti = [
+  { label: "Stadio", value: "stadio" },
+  { label: "Palazzetto", value: "palazzetto" },
+];
+const SocietaForm = () => {
+  const [dataDiNascita, setDataDiNascita] = useState("");
+  const [nome, setNome] = useState("");
+  const [luogo, setLuogo] = useState("");
+  const [stabilimento, setStabilimento] = useState([]);
+  const [lega, setLega] = useState("");
+  const [posizione, setPosizione] = useState(0);
+  const [trofeiVinti, setTrofeiVinti] = useState(0);
+
+  return (
+    <div>
+      <label for="name">Nome Società</label>
+      <input
+        name="name"
+        placeholder="Nome Società"
+        type="text"
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <label for="Fondazione ">Fondazione Società</label>
+      <input
+        name="Fondazione "
+        placeholder="Fondazione Società"
+        type="date"
+        onChange={(e) => setDataDiNascita(e.target.value)}
+      />
+      <label for="Luogo">Luogo</label>
+      <input
+        name="Luogo"
+        placeholder="Luogo"
+        type="text"
+        onChange={(e) => setLuogo(e.target.value)}
+      />
+
+      <MultiSelect
+        options={stabilimenti}
+        value={stabilimento}
+        onChange={setStabilimento}
+        labelledBy="Seleziona stabilimento"
+        hasSelectAll={false}
+      />
+
+      <label for="Lega ">Lega </label>
+      <input
+        name="Lega "
+        placeholder="Lega "
+        type="text"
+        onChange={(e) => setLega(e.target.value)}
+      />
+      <label for="Posizione">Posizione</label>
+      <input
+        name="Posizione"
+        placeholder="Posizione"
+        type="number"
+        onChange={(e) => setPosizione(e.target.value)}
+      />
+      <div>
+        <p>Info</p>
+        <label for="TrofeiVinti">Trofei Vinti</label>
+        <input
+          name="TrofeiVinti"
+          placeholder="Trofei Vinti"
+          type="number"
+          onChange={(e) => setTrofeiVinti(e.target.value)}
+        />
+      </div>
+
+      <button>Perfect Bitch</button>
+    </div>
+  );
+};
+
+export default SocietaForm;
