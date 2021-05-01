3e4ef28d5b81eebccc46c0c71c351d509d6e552b
davideVitiello <lgvitiello9@gmail.com>
2021-05-01 02:18:03 +0200

    Server per le opzioni che potrebbero servire al giro

---
+++ src/components/Login/AggiungiInfo/OpzioneForm/Opzione.jsx
@@ -0,0 +1,13 @@
+const Opzione = ({ nome, icona, setSelezionato, otherStyles }) => {
+  const stile = { cursor: "pointer", ...otherStyles };
+  const handleClick = () => {
+    setSelezionato(nome);
+  };
+  return (
+    <div style={stile} onClick={handleClick}>
+      <p>{nome}</p>
+      {icona && <img src={icona} />}
+    </div>
+  );
+};
+export default Opzione;
