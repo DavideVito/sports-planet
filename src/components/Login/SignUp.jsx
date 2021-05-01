557434d68d32340f3a87e4e66cbabd71ee0b976a
davideVitiello <lgvitiello9@gmail.com>
2021-05-01 02:17:09 +0200

    Creato il form di accesso con tutti gli input

--- src/components/Login/SignUp.jsx
+++ src/components/Login/SignUp.jsx
@@ -1,30 +1,21 @@
 import React, { useState } from "react";
 import { signup, login, continua } from "./LoginMiddleware";
+import SigninForm from "./SigninForm";
 import { useAuth } from "reactfire";
 import firebase from "firebase/app";
 import Errore from "../Errore";
 
 const SignUp = () => {
   const auth = useAuth();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+
   const [error, setError] = useState();
 
   const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-  const signUpHandler = async () => {
-    try {
-      let ris = await signup(auth, { email, password });
-      continua(ris);
-    } catch (error) {
-      console.log(error);
-      setError(error);
-    }
-  };
-
   const signUpWithGoogle = async () => {
     try {
       let ris = await login(auth, googleAuthProvider);
+
       continua(ris);
     } catch (error) {
       console.log(error);
@@ -34,7 +25,7 @@ const SignUp = () => {
 
   return (
     <div>
-      Registrati negro
+      Registrazione
       {error?.code === "auth/invalid-email" ? (
         <Errore
           tilolo="Email Sbagliata"
@@ -51,22 +42,16 @@ const SignUp = () => {
       ) : (
         <></>
       )}
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          placeholder="Username"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={signUpHandler}>Sign up</button>
-      </form>
       <div>
-        <button onClick={signUpWithGoogle}>Sign up with google</button>
+        <button onClick={signUpWithGoogle}>Registrati con Google</button>
       </div>
+      <SigninForm
+        signup={signup}
+        continua={continua}
+        auth={auth}
+        error={error}
+        setError={setError}
+      />
     </div>
   );
 };
---
+++ src/components/Login/SigninForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import Errore from "../Errore";
+
+const SigninForm = ({ signup, continua, auth, error, setError }) => {
+  const signUpHandler = async () => {
+    try {
+      let ris = await signup(auth, { email, password, nome, cognome });
+
+      continua(ris);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nome, setNome] = useState("");
+  const [cognome, setCognome] = useState("");
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          signUpHandler();
+        }}
+      >
+        <input
+          required
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <br />
+        <input
+          required
+          type="text"
+          placeholder="Cognome"
+          onChange={(e) => setCognome(e.target.value)}
+        />
+        <br />
+        <input
+          required
+          type="mail"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          required
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Sign up</button>
+      </form>
+    </>
+  );
+};
+
+export default SigninForm;
