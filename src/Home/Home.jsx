63aca42895aa20ff556b24ba2dd37b63af3b2fd6
michele_flore <michele.f@pixelfoundry.studio>
2021-09-06 15:47:15 +0200

    adding content and style to home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -33,6 +33,13 @@ import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import CardActionArea from '@material-ui/core/CardActionArea';
+import AllInboxIcon from '@material-ui/icons/AllInbox';
+import StarIcon from '@material-ui/icons/Star';
+import AddIcon from '@material-ui/icons/Add';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -103,7 +110,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Home = () => {
+
+const ScrollableTabsButtonForce = () => {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
@@ -116,32 +124,32 @@ const Home = () => {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar  position="static" color="default">
         <Tabs
-          variant="fullWidth"
           value={value}
           onChange={handleChange}
-          aria-label="nav tabs example"
+          variant="fullWidth"
+          scrollButtons="on"
+          indicatorColor="primary"
+          textColor="primary"
+          aria-label="scrollable force tabs example"
         >
+          <Tab label="Per te" icon={<AllInboxIcon />} {...a11yProps(0)} />
+          <Tab label="Seguiti" icon={<StarIcon />} {...a11yProps(1)} />
 
-          <LinkTab label="Home" href="/home" {...a11yProps(0)} />
-          <LinkTab label="Cerca" href="/cerca" {...a11yProps(1)} />
-          <LinkTab label="Posta" href="/posta" {...a11yProps(2)} />
-          <LinkTab label="Profilo" href="/profilo" {...a11yProps(3)} />
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
-        <AuthCheck fallback={<Login />}>
-          <ShowPost />
-        </AuthCheck>
+      <TabPanel class="secondary-tabs" value={value} index={0}>
+
         <div class="card_wrapper">
           <Card className={classes.root}>
             <CardHeader
               avatar={
                 <Avatar aria-label="recipe" className={classes.avatar}>
-                  R
+                  M
                 </Avatar>
               }
               action={
@@ -187,6 +195,144 @@ const Home = () => {
             </Collapse>
           </Card>
         </div>
+
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <h2>Qui troverai tutti i post degli utenti seguiti</h2>
+        <span>Clicca l'icona <StarIcon></StarIcon> per aggiungere un utente ai preferiti ricevendo notifiche e visualizzando tutti i suoi post</span>
+      </TabPanel>
+
+    </div>
+  );
+}
+
+const ShowPost = () => {
+  const { data: user } = useUser();
+  const [post, setPost] = useState([]);
+  const [showAddPost, setShowAddPost] = useState(false);
+  const firestore = useFirestore();
+  const userDataQuery = firestore.collection("Giocatori").doc(user.uid);
+
+  const userData = useFirestoreDocDataOnce(userDataQuery);
+
+  useEffect(() => {
+    if (!userData) {
+      return;
+    }
+    const utentiSeguiti = userData.utentiSeguiti;
+
+    let oggi = new Date();
+    let ieri = new Date(new Date().setDate(oggi.getDate() - 3));
+
+    if (!utentiSeguiti) {
+      return;
+    }
+
+    const promises = utentiSeguiti.map(async (utente) => {
+      return await firestore
+        .collection("Giocatori")
+        .doc(utente)
+        .collection("Posts")
+        .where("dataPostato", ">", ieri)
+        .where("dataPostato", "<=", oggi)
+        .orderBy("dataPostato", "desc")
+        .orderBy("like", "desc")
+        .limit(30)
+        .get();
+    });
+
+    Promise.all(promises).then((documents) => {
+      let ogg = [];
+
+      documents.forEach((doc) => {
+        let a = doc.docs.map((d) => {
+          return { id: d.id, ...d.data() };
+        });
+
+        ogg = [...a, ...ogg];
+      });
+
+      documents = ogg;
+      console.table(documents);
+      setPost(documents);
+    });
+  }, []);
+
+  return (
+    <div class="heading-buttons">
+          <Button
+            variant="outlined"
+            color="primary"
+            onClick={() => (window.location.href = "/chats")}
+          >
+            Vai alle chat
+          </Button>
+          <Button
+            color="primary"
+            variant="outlined"
+            onClick={(e) => {
+              setShowAddPost(!showAddPost);
+            }}
+          >
+            {!showAddPost ? <div>Aggiungi un post</div> : <div>Chiudi</div>}
+          </Button>
+
+          {showAddPost && <AddPost user={user} />}
+
+        {post.length === 0 ? (
+          <div style={{ display: "none" }}></div>
+        ) : (
+          <div>
+            {post.map((p) => {
+              return <Post key={p.id} post={p} user={user} />;
+            })}
+          </div>
+        )}
+    </div>
+  );
+};
+
+const Home = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const [valueChild, setValueChild] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeChild = (event, newValueChild) => {
+    setValueChild(newValueChild);
+  };
+
+
+  
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          onChange={handleChange}
+          aria-label="nav tabs example"
+        >
+
+          <LinkTab icon={<HomeIcon />} label="Home" href="/home" {...a11yProps(0)} />
+          <LinkTab icon={<SearchIcon />} label="Cerca" href="/cerca" {...a11yProps(1)} />
+          <LinkTab icon={<AddIcon />} label="Posta" href="/posta" {...a11yProps(2)} />
+          <LinkTab icon={<AccountCircleIcon />} label="Profilo" href="/profilo" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+
+      <TabPanel value={value} index={0}>
+
+        <AuthCheck fallback={<Login />}>
+          <ShowPost />
+        </AuthCheck>
+
+        <ScrollableTabsButtonForce></ScrollableTabsButtonForce>
+
+
 
       </TabPanel>
       <TabPanel value={value} index={1}>
@@ -284,96 +430,5 @@ const Home = () => {
   );
 };
 
-const ShowPost = () => {
-  const { data: user } = useUser();
-  const [post, setPost] = useState([]);
-  const [showAddPost, setShowAddPost] = useState(false);
-  const firestore = useFirestore();
-  const userDataQuery = firestore.collection("Giocatori").doc(user.uid);
-
-  const userData = useFirestoreDocDataOnce(userDataQuery);
-
-  useEffect(() => {
-    if (!userData) {
-      return;
-    }
-    const utentiSeguiti = userData.utentiSeguiti;
-
-    let oggi = new Date();
-    let ieri = new Date(new Date().setDate(oggi.getDate() - 3));
-
-    if (!utentiSeguiti) {
-      return;
-    }
-
-    const promises = utentiSeguiti.map(async (utente) => {
-      return await firestore
-        .collection("Giocatori")
-        .doc(utente)
-        .collection("Posts")
-        .where("dataPostato", ">", ieri)
-        .where("dataPostato", "<=", oggi)
-        .orderBy("dataPostato", "desc")
-        .orderBy("like", "desc")
-        .limit(30)
-        .get();
-    });
-
-    Promise.all(promises).then((documents) => {
-      let ogg = [];
-
-      documents.forEach((doc) => {
-        let a = doc.docs.map((d) => {
-          return { id: d.id, ...d.data() };
-        });
-
-        ogg = [...a, ...ogg];
-      });
-
-      documents = ogg;
-      console.table(documents);
-      setPost(documents);
-    });
-  }, []);
-
-  return (
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <Button
-            variant="outlined"
-            color="primary"
-            onClick={() => (window.location.href = "/chats")}
-          >
-            Vai alle chat
-          </Button>
-        </div>
-        <div class="col">
-          <Button
-            color="primary"
-            variant="outlined"
-            onClick={(e) => {
-              setShowAddPost(!showAddPost);
-            }}
-          >
-            {!showAddPost ? <div>Aggiungi un post</div> : <div>Chiudi</div>}
-          </Button>
-        
-          {showAddPost && <AddPost user={user} />}
-          
-        </div>
-        {post.length === 0 ? (
-          <div style={{ display: "none" }}></div>
-        ) : (
-          <div>
-            {post.map((p) => {
-              return <Post key={p.id} post={p} user={user} />;
-            })}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
 
 export default Home;
